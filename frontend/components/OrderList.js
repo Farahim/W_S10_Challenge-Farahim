b44c9d492dbc965d16bca775a094860197ff0d2c
Farahim Ibrahimli <i.farahim@gmail.com>
2024-10-24 23:15:51 -0400

    done

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,35 +1,70 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState} from 'react'
+
+let getUrl = 'http://localhost:9009/api/pizza/history';
+
+
+
+
 
 export default function OrderList() {
-  const orders = []
-  return (
-    <div id="orderList">
-      <h2>Pizza Orders</h2>
-      <ol>
-        {
-          orders.map(() => {
-            return (
-              <li key={1}>
-                <div>
-                  order details here
-                </div>
-              </li>
-            )
-          })
+
+    const [orders, setOrders] = useState([]);
+    const [filter, setFilter] = useState('All');
+
+    axios
+        .get(getUrl)
+        .then((res) => {setOrders(res.data);})
+        .catch((err) => console.log(err));
+
+
+    function setSizeFilter(event) {
+        setFilter(event.target.textContent);
+    }
+
+
+    function makeOrderString(order) {
+        let result = order.customer;
+        result += " ordered a size ";
+        result += order.size;
+        result += " with ";
+        if(order.toppings) {
+            result += order.toppings.length;
+            result += " topping";
+            if(order.toppings.length > 1) {
+                result += "s";
+            }
+        } else {
+            result += "no toppings";
         }
-      </ol>
-      <div id="sizeFilters">
+        return result;
+    }
+    return (
+        <div id="orderList">
+            <h2>Pizza Orders</h2>
+            @@ -32,24 +47,24 @@
+            return (
+            <li key={order.id}>
+                <div>
+                    {order.customer} ordered a size {order.size} with {order.toppings?order.toppings.length:"no"} topping{order.toppings?order.toppings.length==1?"":"s":"s"}
+                    {makeOrderString(order)}
+                </div>
+            </li>
+            )})        }
+        </ol>
+    <div id="sizeFilters">
         Filter by size:
         {
-          ['All', 'S', 'M', 'L'].map(size => {
-            const className = `button-filter${size === 'All' ? ' active' : ''}`
-            return <button
-              data-testid={`filterBtn${size}`}
-              className={className}
-              key={size}>{size}</button>
-          })
+            ['All', 'S', 'M', 'L'].map(size => {
+                const className = `button-filter${size === filter ? ' active' : ''}`
+                return <button onClick={setSizeFilter}
+                               data-testid={`filterBtn${size}`}
+                               className={className}
+                               key={size}>{size}
+                </button>
+            })
         }
-      </div>
     </div>
-  )
+</div>
+)
 }
--- frontend/components/PizzaForm.js
+++ frontend/components/PizzaForm.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState} from 'react'
 
 const initialFormState = { // suggested
   fullName: '',
@@ -10,56 +11,90 @@ const initialFormState = { // suggested
   '5': false,
 }
 
+let postUrl = 'http://localhost:9009/api/pizza/order'
+
 export default function PizzaForm() {
+  const [fullName, setFullName] = useState(initialFormState.fullName);
+  const [size, setSize] = useState(initialFormState.size);
+  const [toppings, setToppings] = useState([]);
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
+  const [message, setMessage] = useState('');
+
+  function handleChangeFullName (event) {
+    setFullName(event.target.value);
+  }
+
+  function handleChangeSize(event) {
+    setSize(event.target.value);
+  }
+
+  function handleChangeToppings() {
+    let newToppingsList = [];
+    const toppingsInput = document.getElementById('toppingsInput').querySelectorAll('label');
+    for(let i = 0; i < toppingsInput.length; i++) {
+      let t = toppingsInput[i].querySelector('input');
+      if(t.checked) {
+        newToppingsList.push(t.name);
+      }
+    }
+
+    setToppings(newToppingsList);
+  }
+
+  function submitOrder(event) {
+    event.preventDefault();
+    setSuccess(true);
+    setMessage('Order in progress...');
+    let orderObj = {
+      fullName: fullName,
+      size: size,
+      toppings: toppings
+    };
+    axios
+        .post(postUrl, orderObj)
+        .then((res) => {
+          setSuccess(true);
+          setFailure(false);
+          setMessage(res.data.message);
+        })
+        .catch((err) => {
+          setSuccess(false);
+          setFailure(true);
+          setMessage(err.response.data.message)
+        });
+    setFullName('');
+    document.getElementById('fullName').value='';
+    setToppings([]);
+    const toppingsInput = document.getElementById('toppingsInput').querySelectorAll('label');
+    for(let i = 0; i < toppingsInput.length; i++) {
+      let t = toppingsInput[i].querySelector('input');
+      t.checked = false;
+    }
+    setSize('');
+    document.getElementById('size').value = '';
+
+  }
+
   return (
-    <form>
-      <h2>Pizza Form</h2>
-      {true && <div className='pending'>Order in progress...</div>}
-      {true && <div className='failure'>Order failed: fullName is required</div>}
+      <form>
+        <h2>Pizza Form</h2>
+        {success && <div className='pending'>{message}</div>}
+        {failure && <div className='failure'>Order failed: {message}</div>}
 
-      <div className="input-group">
-        <div>
-          <label htmlFor="fullName">Full Name</label><br />
-          <input
-            data-testid="fullNameInput"
-            id="fullName"
-            name="fullName"
-            placeholder="Type full name"
-            type="text"
-          />
+        <div className="input-group">
+          <div>
+            <label htmlFor="fullName">Full Name</label><br />
+            <input
+                data-testid="fullNameInput"
+                id="fullName"
+                name="fullName"
+                placeholder="Type full name"
+                type="text"
+                onChange={handleChangeFullName}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="input-group">
-        <div>
-          <label htmlFor="size">Size</label><br />
-          <select data-testid="sizeSelect" id="size" name="size">
-            <option value="">----Choose size----</option>
-            <option value="S">Small</option>
-            <option value="M">Medium</option>
-            <option value="L">Large</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="input-group">
-        <label>
-          <input data-testid="checkPepperoni" name="1" type="checkbox" />
-          Pepperoni<br /></label>
-        <label>
-          <input data-testid="checkGreenpeppers" name="2" type="checkbox" />
-          Green Peppers<br /></label>
-        <label>
-          <input data-testid="checkPineapple" name="3" type="checkbox" />
-          Pineapple<br /></label>
-        <label>
-          <input data-testid="checkMushrooms" name="4" type="checkbox" />
-          Mushrooms<br /></label>
-        <label>
-          <input data-testid="checkHam" name="5" type="checkbox" />
-          Ham<br /></label>
-      </div>
-      <input data-testid="submit" type="submit" />
-    </form>
-  )
-}
+        <div className="input-group">
+          <div>
